88f8368df3f3b935dfed80e243039cd2c5417857
vjcajilemac <vjcajilema@gmail.com>
2023-12-30 18:02:43 -0500

    get data

---
+++ src/context/types/appTypes.js
@@ -0,0 +1,4 @@
+export const appTypes = {
+    getTask:  '[Task] get',
+
+}
--- src/pages/TaskPage/TaskPage.tsx
+++ src/pages/TaskPage/TaskPage.tsx
@@ -1,31 +1,38 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import TaskLayout from '../../layout/TaskLayout'
 import { TaskList } from '../../components'
 import {TaskInterface} from '../../interfaces/Task'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faList } from '@fortawesome/free-solid-svg-icons'
 import './TaskPage.css'
+import { ApiGet } from '../../helpers/apiHelpers'
 
 const TaskPage = () => {
+  const [state, setState] = useState({
+    tasks:[]
+  })
+  const {tasks} = state;
   const taskArrayData: TaskInterface[] =[
     {id:1, name:"Crear Componentes", due_date:"18/12/2023", observation:"saldlaskdlsakaaaaaaaaaaaaaaaaaaaa", status:{name:"cumplido", color:"blue"}},
     {id:2, name:"Crear Backend", due_date:"18/12/2023", observation:"", status:{name:"activo", color:"green"}}];
     useEffect(() => {
-      Hello()
+      initData()
     }, [])
     
-    const Hello = () =>{
-        console.log('asd')
+    const initData = async () =>{
+      const dataResponse = await ApiGet('tasks');
+      setState({...state, tasks: dataResponse});
+      console.log("response", dataResponse)
     }
   return (
-    <TaskLayout taskList = {taskArrayData}>
+    <TaskLayout taskList = {tasks}>
       <div className='task_content__container'>
         <div className='task_content__tittle'>
         <FontAwesomeIcon icon={faList} /><h1>Tareas</h1>
         </div>      
 
 
-        <TaskList taskList = {taskArrayData}></TaskList>
+        <TaskList taskList = {tasks}></TaskList>
        
       </div>
  
